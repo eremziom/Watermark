632b6dbf989dee973d60269d5574a95fac447bb6
Maciej Kryczka <maciejkryczka@hotmail.com>
2020-01-03 13:46:50 +0100

    add try...catch to watermark functions

--- app.js
+++ app.js
@@ -3,30 +3,40 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 
 const addTextWatermarkToImage = async function(inputFile, outputFile, text) {
-  const image = await Jimp.read(inputFile);
-  const font = await Jimp.loadFont(Jimp.FONT_SANS_32_WHITE);
-  const textData = {
-    text,
-    alignmentX: Jimp.HORIZONTAL_ALIGN_CENTER,
-    alignmentY: Jimp.VERTICAL_ALIGN_MIDDLE,
-  };
-  image.print(font, 0, 0, textData, image.getWidth(), image.getHeight());
-  image.quality(100).write(outputFile);
+  try {
+    const image = await Jimp.read(inputFile);
+    const font = await Jimp.loadFont(Jimp.FONT_SANS_32_WHITE);
+    const textData = {
+      text,
+      alignmentX: Jimp.HORIZONTAL_ALIGN_CENTER,
+      alignmentY: Jimp.VERTICAL_ALIGN_MIDDLE,
+    };
+    image.print(font, 0, 0, textData, image.getWidth(), image.getHeight());
+    image.quality(100).write(outputFile);
+  }
+  catch {
+    console.log('Something went wrong... Try again!');
+  }
 };
 
 //addTextWatermarkToImage('./img/test.jpg', './img/test-with-watermark.jpg', 'Hello world')
 
 const addImageWatermarkToImage = async function(inputFile, outputFile, watermarkFile){
-  const image = await Jimp.read(inputFile);
-  const watermark = await Jimp.read(watermarkFile);
-  const x = image.getWidth() / 2 - watermark.getWidth() / 2;
-  const y = image.getHeight() / 2 - watermark.getHeight() / 2;
+  try {
+    const image = await Jimp.read(inputFile);
+    const watermark = await Jimp.read(watermarkFile);
+    const x = image.getWidth() / 2 - watermark.getWidth() / 2;
+    const y = image.getHeight() / 2 - watermark.getHeight() / 2;
 
-  image.composite(watermark, x, y, {
-    mode: Jimp.BLEND_SOURCE_OVER,
-    opacitySource: 0.5,
-  });
-  image.quality(100).write(outputFile);
+    image.composite(watermark, x, y, {
+      mode: Jimp.BLEND_SOURCE_OVER,
+      opacitySource: 0.5,
+    });
+    image.quality(100).write(outputFile);
+  }
+  catch(error) {
+    console.log('Something went wrong... Try again!');
+  }
 };
 
 //addImageWatermarktoImage('./img/test.jpg', './img/test-with-watermark2.jpg', './img/logo.jpg')
